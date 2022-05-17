5f18c03b9da03f31ae097a14a6f1307c4150aab8
ure <ure@moreorless.be>
2022-05-17 13:33:56 +0200

    added boot tasks

--- code.py
+++ code.py
@@ -1,5 +1,9 @@
 # Need to implement threading on one button to select (load/unload different modules)
+import asyncio
 import supervisor
+
+
+
 if supervisor.runtime.usb_connected:
   import PC
 else:
